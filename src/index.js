b6112e96ebdbdcc529e8aba3c86b8ae1affc72ef
Shakti Singh <30585570+shakti97@users.noreply.github.com>
2019-02-03 13:54:13 +0530

    useEffect and Custom Hooks

--- src/index.js
+++ src/index.js
@@ -4,15 +4,36 @@ import { useState, useEffect } from "react";
 
 import "./styles.css";
 
+function FriendStatus(props) {
+  
+  
+  if (props.isOnline === null) {
+    return 'Loading...';
+  }
+  return props.isOnline ? <div>'Online'</div> : <div>'Offline'</div>;
+}
+
 function App() {
   const [count, setCount] = useState(0);
+  const [isOnline, setIsOnline] = useState(null);
+
   useEffect(() => {
-    console.log("count ", count);
-  });
+    console.log('hey subscription');
+    handleStatusChange({ isOnline: true });
+    return function cleanup() {
+      console.log('hey unsubscription');
+      handleStatusChange({ isOnline: false });
+    }
+  },[isOnline]);
+  function handleStatusChange(status) {
+    setIsOnline(status.isOnline);
+  }
   return (
     <div className="App">
       <p>you clicked me {count} times</p>
       <button onClick={() => setCount(count + 1)}>click me</button>
+      <FriendStatus {...{isOnline}}/>
+      <button onClick={()=>handleStatusChange({isOnline:!isOnline})}>Change Status</button>
     </div>
   );
 }
